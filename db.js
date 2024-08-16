72ad852ae74a5be982a02638bbefd231bbe8072e
hotenabc <hotenabc789@gmail.com>
2024-08-16 21:16:33 +0700

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const uri = "mongodb+srv://admin:example@products.0zociic.mongodb.net/?retryWrites=true&w=majority";
 const connectDB = async () => {
     try {
-        await mongoose.connect(uri , 
+        await mongoose.connect(uri, 
         {useNewUrlParser: true, 
-        useUnifiedTopology: true
-    });
-    console.log("MongoDB Connected")
+        useUnifiedTopology: true}
+        );
+        console.log("MongoDB Connected")
     }
     catch (error) {
         console.error(error.message);
